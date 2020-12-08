6fc67f64e4dc35668600d33550182c4273882b99
reynord <smraihankabir07@gmail.com>
2020-12-09 05:24:33 +0600

    main

--- app/admin.py
+++ app/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import Contact
+from import_export.admin import ImportExportModelAdmin
 
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name','email','phone','info','user')
-    list_editable = ('info',) 
+class ContactAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'name', 'gender', 'email', 'info', 'phone')
+    list_display_links = ('id','name')
+    list_editable = ('info',)
     list_per_page = 10
-    search_fields = ('name','email','phone','info','user')
-    list_filter = ('user', 'date_added')
+    search_fields = ('name', 'gender', 'email', 'info', 'phone')
+    list_filter = ('gender', 'date_added')
+
 
 admin.site.register(Contact, ContactAdmin)
 admin.site.unregister(Group)
